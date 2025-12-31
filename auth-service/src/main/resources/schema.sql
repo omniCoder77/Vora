87f7dbde9036b324be7d126ca927ff4eb8d26ea5
omniCoder77 <rishabhsaraswat17@gmail.com>
2025-12-31 23:00:25 +0530

    Fixed schema: Added mfa_secret column

--- auth-service/src/main/resources/schema.sql
+++ auth-service/src/main/resources/schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE users
     email           VARCHAR(255) NOT NULL UNIQUE,
     hashed_password TEXT         NOT NULL,
     created_at      TIMESTAMPTZ  NOT NULL DEFAULT now(),
-    mfa_options     TEXT
+    mfa_options     TEXT,
+    mfa_secret      TEXT
 );
